6670ec10f94b38a900f31334e121e73622f71dba
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2021-02-03 15:53:24 +0100

    Add module prefix

--- executeAll.sh
+++ executeAll.sh
@@ -77,13 +77,13 @@ fi
 
 #Check, if a slowdown is detected for App#test
 (
-	state=$(grep -A21 '"call" : "com.example.android_example.TestMe#test",' results/$version/com.example.android_example.ExampleUnitTest_test_TestMe.js | grep '"state" : "SLOWER",' | grep -o 'SLOWER')
+	state=$(grep -A21 '"call" : "com.example.android_example.TestMe#test",' results/$version/app§com.example.android_example.ExampleUnitTest_test_TestMe.js | grep '"state" : "SLOWER",' | grep -o 'SLOWER')
 	if [ "$state" != "SLOWER" ]
 		then
 			echo "State for TestMe#test in com.example.android_example.ExampleUnitTest_test_TestMe.js has not the expected value SLOWER!"
 			echo "Found value for state: $state"
 			echo ":::::::::::::::::::ExampleUnitTest_test_TestMe.js:::::::::::::::::::::"
-			cat results/$version/com.example.android_example.ExampleUnitTest_test_TestMe.js
+			cat results/$version/app§com.example.android_example.ExampleUnitTest_test_TestMe.js
 			exit 1
 		else
 			echo "Slowdown is detected for TestMe#test."
